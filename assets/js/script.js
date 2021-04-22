1da9dcd4e36ce9e380760db3e8385876f33fa766
bigoroberto <bigoroberto@gmail.com>
2021-04-22 20:20:01 +0200

    correzione esercizio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
-$(function(){
+// dropdown
+$(".parent-dropdown").mouseenter(function(){
+  //console.log('mouseenter su parent');
+  $(".dropdown").removeClass("active");
+  $(this).children(".dropdown").addClass("active");
+});
 
-  $(document).on('click', '.header-r ul .with-dropdown > a', function(){
 
-    $(this).next().toggleClass('active')
-    
-  });
+$(".parent-dropdown").click(function(){
+  //console.log('mouseenter su parent');
+  $(this).children(".dropdown").toggleClass("active");
+});
 
+
+
+
+
+
+
+
+
+// sezione lingua
+$(".arrow").mouseenter(function(){
+  //console.log('mouseenter su arrow');
+  $(".fas").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+});
+
+$(".arrow").mouseleave(function(){
+  //console.log('mouseleave su arrow');
+  $(".fas").removeClass("fa-chevron-up").addClass("fa-chevron-down");
 });
